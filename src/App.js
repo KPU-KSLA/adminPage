e3eee2b4a624d77787f4bf975cbebc16e70f2127
jiho lee <optional.int@kakao.com>
2020-12-08 04:28:23 +0900

    Add auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase'
+const firebaseui = require('firebaseui')
 
-function App() {
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWqJzzQnNENKkOVpsOhWlITGv-Um9Y_Hc',
+  authDomain: 'fir-auth-sample-page.firebaseapp.com',
+  projectId: 'fir-auth-sample-page',
+  storageBucket: 'fir-auth-sample-page.appspot.com',
+  messagingSenderId: '973196040928',
+  appId: '1:973196040928:web:b66fd30bd80805bcc94e85',
+  measurementId: 'G-BLQ12S759K'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+function Auth () {
+  const [isSignedIn, setIsSignedIn] = useState(false) // Local signed-in state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user)
+    })
+    return () => unregisterAuthObserver() // Make sure we un-register Firebase observers when the component unmounts.
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  )
 }
 
-export default App;
+function Content () {
+  return (
+    <span>
+      Hello, signed user!
+    </span>
+  )
+}
+function App () {
+  return (
+    <div className="App">
+      <Auth></Auth>
+    </div>
+  )
+}
+
+export default App
