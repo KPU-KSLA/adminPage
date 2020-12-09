7fca14a7fc972204b6743320645fee560de184e8
jiho lee <optional.int@kakao.com>
2020-12-09 22:01:18 +0900

    Add Manage students

---
+++ src/ManageStudents.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ManageStudents ({ timeCount, lectureRoom }) {
+  return (
+        <div></div>
+  )
+}
+
+ManageStudents.propTypes = {
+  timeCount: PropTypes.number,
+  lectureRoom: PropTypes.string
+}
+
+export default ManageStudents
