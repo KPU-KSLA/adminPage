9c23e551101d52e8d7e061de4d8b6d730fbe0116
jiho lee <optional.int@kakao.com>
2020-12-09 14:20:41 +0900

    Change App structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React, { useState } from 'react'
+import React from 'react'
 import Auth from './Auth'
-import Login from './Login'
 import Navbar from './Navbar'
 
 function App () {
-  const [currentPage, setCurrentPage] = useState(Login)
   return (
-    <div className="App">
-      <Navbar setCurrentPage={(e) => setCurrentPage(e)} />
-      <Auth currentPage={currentPage} />
+    <div className="App container fill center-block">
+      <Navbar />
+      <Auth />
     </div>
   )
 }
