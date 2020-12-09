e6d4c023060d1d6600f7e58adca5f9d384fdf31c
jiho lee <optional.int@kakao.com>
2020-12-09 14:22:24 +0900

    Fix Login

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase'
+import PropTypes from 'prop-types'
+import IndexPage from './IndexPage'
 
-function Login () {
+function Login ({ isSigned, setIsSigned, uiConfig }) {
+  console.log(uiConfig)
+  if (isSigned) {
+    return (<IndexPage isSigned={isSigned} />)
+  }
   return (
-        <p>Login</p>
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
   )
+}
+
+Login.propTypes = {
+  isSigned: PropTypes.bool,
+  setIsSigned: PropTypes.func,
+  uiConfig: PropTypes.object
 }
 
 export default Login
