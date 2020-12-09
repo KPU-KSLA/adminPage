37b083bf33e097ed335c5e52a35cffb9dd7bb3ba
jiho lee <optional.int@kakao.com>
2020-12-09 14:22:16 +0900

    Improved Show a little

--- src/Show.js
+++ src/Show.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Jumbo from './Jumbo'
 
-function Show () {
+function Show ({ isSigned }) {
+  const content = isSigned ? '1234' : '출석현황을 보기 위해서는 로그인해야 합니다.'
+  const renderedJumbo = <Jumbo content="KPU 출석현황 페이지"></Jumbo>
   return (
-        <p>Show</p>
+    <div>
+      {renderedJumbo}
+        <p className="text-center">{content}</p>
+    </div>
   )
+}
+
+Show.propTypes = {
+  isSigned: PropTypes.bool
 }
 
 export default Show
