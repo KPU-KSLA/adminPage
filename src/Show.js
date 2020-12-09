b5268f774c68847d4b93e7147fe4c89d9dad97ca
jiho lee <optional.int@kakao.com>
2020-12-09 21:46:03 +0900

    Change comment

--- src/Show.js
+++ src/Show.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import GetInput from './GetInput'
 import ShowStudents from './ShowStudents'
 
-// a function as Man in the middle
+// Role as Router
 
 function Show ({ isSigned }) {
   const notSignedInText = '출석현황을 보기 위해서는 로그인해야 합니다.'
