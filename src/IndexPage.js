cc920088fb9755fb45c38fd33709e4bb496afa09
jiho lee <optional.int@kakao.com>
2020-12-09 22:01:11 +0900

    Add Welcome message

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -4,10 +4,12 @@ import Jumbo from './Jumbo'
 
 function IndexPage (props) {
   const isSigned = props?.isSigned ?? false
+  const welcome = 'KPU 출석관리에 오신 것을 환영합니다.'
   const content = isSigned ? '내비바를 선택해 주세요.' : '로그인해 주세요.'
   return (
     <div>
-        <Jumbo content="KPU 출석관리 어드민 페이지"></Jumbo>
+      <Jumbo content="KPU 출석관리 어드민 페이지"></Jumbo>
+        <p className="text-center">{welcome}</p>
         <p className="text-center">{content}</p>
     </div>
   )
